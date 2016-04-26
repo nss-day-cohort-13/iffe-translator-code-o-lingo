e41b0dd24fc6cf23f2beaeb8a66b9c801eceb918
Hamilton Harris <hamilton.carter.harris@gmail.com>
2016-04-26 15:07:35 -0500

    created dom.js page added the dom variables

---
+++ dom.js
@@ -0,0 +1,26 @@
+// main content
+var mainContent = document.getElementById('content');
+
+
+// user input in the textarea field
+var userInput = document.getElementById('message');
+
+
+// danish radio button
+var danishPerson = document.getElementById('danish');
+
+
+// french radio button
+var frenchPerson = document.getElementById('french');
+
+
+// spanish radio button
+var spanishPerson = document.getElementById('spanish');
+
+
+// translate button
+var translateButton = document.getElementById('button').addEventListener("click", Lang.transLang);
+
+
+// users translated output
+var userOutput = document.getElementById('output');
