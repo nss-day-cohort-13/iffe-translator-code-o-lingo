c1d60aa254bc6d25168d21150d4086ff55f7732b
Chase Ramsey <ramsey.chase@gmail.com>
2016-04-27 11:47:01 -0500

    Added translate funciton to dom.js; called translate function in danish.js

--- danish.js
+++ danish.js
@@ -2,24 +2,22 @@
 var Lang = (function() {
 
 	var danish = {
-		happy: "god",
-		bastille: "bastille",
-		day: "dag"
+		happy: "glad",
+		bastille: "bastilledagen",
+		day: "",
+		to: "til",
+		someone: "nogen",
+		still: "stadig",
+		stuck: "fast",
+		in: "i",
+		a: "et",
+		monarchy: "monarki"
 	};
 
 	return {
-	transDanish: function(input) {
-		var output = "";
-		for (var i = 0; i < input.length; i++) {
-			var current = input[i].toLowerCase();
-			if (current === "") {
-				output;
-			} else {
-					output += danish[current] + " ";
-					printTranslation(output);
-				}
-		};
+		transDanish: function(input) {
+			translate(input, danish);
+		}
 	}
-}
 
 }());
