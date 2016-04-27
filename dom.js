b1533371e04bdd41826b666cc5dc95a967fc5697
Hamilton Harris <hamilton.carter.harris@gmail.com>
2016-04-27 09:14:49 -0500

    renamed dom.js and took away translate.js and saw the eventlistener work in the dev tools

--- dom.js
+++ dom.js
@@ -18,9 +18,24 @@ var frenchPerson = document.getElementById('french');
 var spanishPerson = document.getElementById('spanish');
 
 
-// translate button
-var translateButton = document.getElementById('button').addEventListener("click", Lang.transLang);
+// translate button, once clicked, it will store the value of the textarea into a new variable called "message"
+// next, it checks to see which radio button is checked, then it will run the translate button by
+// passing the users message into the checked IFFE
+var translateButton = document.getElementById('button').addEventListener("click", function(MouseEvent) {
+	console.log("event", MouseEvent);
+	var message = userInput.value;
+	message = message.split(" ");
+	console.log("The users array: ", message);
+	if (frenchPerson.checked) {
+		Lang.transFrench(message);
+	} else if (danishPerson.checked) {
+		Lang.transDanish(message);
+	} else if (spanishPerson.checked) {
+		Lang.transSpanish(message);
+	}
+});
 
+// we need to return 'message'?
 
 // users translated output
 var userOutput = document.getElementById('output');
